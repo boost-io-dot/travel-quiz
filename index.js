1728b462e8567b1a087c04c723abbbb7b366d6dd
boost-io-dot <cchoo1227@gmail.com>
2025-01-30 03:59:44 +0800

    updated

--- index.js
+++ index.js
@@ -6,27 +6,27 @@ let currentQuestion = 0;
 
 let qns = [
   {
-	quizImg: "trip-start.svg",
+	quizImg: "images/trip-start.svg",
 	question: "First things first—where are you going?",
 	answers: ["Wander through the misty highlands of Scotland.", "Explore the bustling streets and markets of Bangkok.", "Soak up the sun and food scene in Malaysia.", "Hit the scenic trails and night markets of Taiwan."]
   },
   {
-	quizImg: "packing.svg",
+	quizImg: "images/packing.svg",
 	question: "How are you helping with trip prep?",
 	answers: ["Packing everything like a pro (you’ve even got snacks!).", "Planning the itinerary—activities, restaurants, and sights.", "Scouting and booking the coolest spots to stay.", "No prep for me—I’ll just show up when it’s time to leave."]
   },
   {
-	quizImg: "activities.svg",
+	quizImg: "images/activities.svg",
 	question: "What’s the first thing you do on vacation?",
 	answers: ["Head to the nearest night market to try all the food.", "Visit a famous sports stadium and take a tour.", "Find a chill bar to vibe and grab a drink.", "Go straight into the wild for some hiking and exploring nature."]
   },
   {
-	quizImg: "distressed.svg",
+	quizImg: "images/distressed.svg",
 	question: "Oh no, your travel pal fell sick! What’s your move?",
 	answers: ["Run out to grab medicine and their favorite snacks.", "Stay by their side and make sure they’re comfy.", "Let them rest while you check in every now and then.", "Time for a mini solo adventure while they recover."]
   },
   {
-	quizImg: "home.svg",
+	quizImg: "images/home.svg",
 	question: "You’re back home. What do you do?",
 	answers: ["Unpack immediately—everything back in its place.", "Only unpack the essentials—the rest can wait.", "Leave your bag sitting there for a week.", "Why unpack? I’m already planning the next trip!"]
   }
